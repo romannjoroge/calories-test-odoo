d59cc83213bc2cd494368654adc9142d9610ae4a
Roman Njoroge <roman.njoroge@njuguna.com>
2026-07-02 15:26:30 +0300

    feat: Defining model to store user calories data
    A users enters gender, age, height, weight, physical activity and goal. Their height and weight (BMR) and their physical activity are used to determine their recommended calories

---
+++ models/calories.py
@@ -0,0 +1,21 @@
+from odoo import models, fields
+
+class UserData(models.Model):
+    _name = "calories.users"
+    _description = "Table to store the data entred by users"
+
+    gender = fields.Selection(selection=[('M', "Male"), ('F', "Female")])
+    age = fields.Integer()
+    height = fields.Float(required=True)
+    weight = fields.Float(required=True)
+    physical_activty = fields.Selection(selection=[
+        ("LIT", "Little exercise"),
+        ("LIG", "Light exercise"),
+        ("M", "Moderate exercise (3-5 days/wk)"),
+        ("V", "Very active"),
+        ("E", "Extra active")
+    ], required=True)
+    goal = fields.Selection(selection=[
+        ("LOSS", "Weight Loss")
+    ])
+

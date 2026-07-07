bc99a8b620f92359f50f49636e2094d39163d0ec
romannjorogeeldama <84716869+romannjoroge@users.noreply.github.com>
2026-07-07 07:25:56 +0300

    feat: Extended product template model
    Extended product template model to also store calories, fat etc for ingredients

---
+++ models/ingredient.py
@@ -0,0 +1,9 @@
+from odoo import model, fields
+
+class Ingredient(model.Models):
+    _inherit = 'product.template'
+
+    calories = fields.Float(string='Calories (kcal)', digits='Product Unit of Measure', help='per 100g')
+    protein = fields.Float(string='Protein (g)', digits='Product Unit of Measure', help='per 100g')
+    fat = fields.Float(string='Fat (g)', digits='Product Unit of Measure', help='per 100g')
+    carbs = fields.Float(string='Carbohydrates (g)', digits='Product Unit of Measure', help='per 100g')
